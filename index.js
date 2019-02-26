02419861264e22f36d1b3b4f61ad0dc73df98c3a
AfulDEV <mahmotoncor@gmail.com>
2019-02-26 11:59:44 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+const Nightmare = require('nightmare');
+
+// waits until every line process for run func
+
+(async () => {
+    for (let i = 0; i < 10; i++) { // loop 10 times
+        const $ = Nightmare({ show: true }) // run new window when everytime return
+        await $.goto('https://duckduckgo.com')
+        await $.type('#search_form_input_homepage', 'github nightmare')
+        await $.click('#search_button_homepage')
+        await $.wait('#r1-0 a.result__a')
+        let href = await $.evaluate(() => document.querySelector('#r1-0 a.result__a').href)
+        console.log(href)
+        await $.end()
+    }
+})();
